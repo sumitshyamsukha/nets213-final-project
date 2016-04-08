37d86756bfe1df422cce391fb12dc3a3f444a445
Sumit <ssumit@seas.upenn.edu>
2016-04-07 23:07:26 -0400

    updated tests to work with quality control

--- src/Test.py
+++ src/Test.py
@@ -1,10 +1,20 @@
 import Aggregator
 import Comment
+import json
+import os
+
+os.chdir('../data')
+file = open(os.getcwd()+"/dummy_data.json")
+data = json.load(file)
 
 a = Aggregator.Aggregator()
-c = Comment.Comment("Hello World", "Sumit")
-d = Comment.Comment("Hello", "Sumit")
-a.add_comment(c)
-d.mark_spam()
-a.add_comment(d)
+
+for comment in data:
+  c = Comment.Comment(comment['text'], comment['poster'])
+  c.upvotes = comment['upvotes']
+  c.downvotes = comment['downvotes']
+  c.spam = comment['isSpam']
+  c.score = c._confidence(c.upvotes, c.downvotes)
+  a.add_comment(c)
+
 a.get_comments()
