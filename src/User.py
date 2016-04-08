bdbfe589ebe05638cf104bb83d83c717efe12dce
Sumit <ssumit@seas.upenn.edu>
2016-04-07 21:39:09 -0400

    added User model

---
+++ src/User.py
@@ -0,0 +1,13 @@
+class User:
+
+  def __init__(self):
+      self.firstName = ""
+      self.lastName = ""
+      self.username = ""
+      self.reputation = 1.0
+      self.pwd = ""
+      self.email = ""
+      self.isVerified = False
+      self.books = []
+      self.comments = []
+
