a4564308effc9b70c317786cc56500dd2b49f692
Sumit <ssumit@seas.upenn.edu>
2016-04-07 22:44:46 -0400

    refactored score update

--- src/Aggregator.py
+++ src/Aggregator.py
@@ -8,19 +8,9 @@ class Aggregator(object):
 
   def add_comment(self, comment):
     if comment is not None:
-      pair = (self._confidence(comment.upvotes, comment.downvotes), comment)
+      pair = (comment.score, comment)
       heapq.heappush(self.comments, pair)
 
   def get_comments(self):
     for c in self.comments:
       print "%f %s" % (c[0], c[1].text)
-
-  def _confidence(self, ups, downs):
-    n = ups + downs
-
-    if n == 0:
-        return 0
-
-    z = 1.96 #1.44 = 85%, 1.96 = 95%
-    phat = float(ups) / n
-    return ((phat + z*z/(2*n) - z * sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n))
--- src/Comment.py
+++ src/Comment.py
@@ -7,3 +7,24 @@ class Comment(object):
       self.score = 1
       self.isSpam = False
       self.poster = poster
+
+  def upvote(self):
+      self.upvotes = self.upvotes + 1
+      self.score()
+
+  def downvote(self):
+      self.downvotes = self.downvotes + 1
+      self.score()
+
+  def score(self):
+      self.score = self._confidence(self.upvotes, self.downvotes)
+
+  def _confidence(self, ups, downs):
+    n = ups + downs
+
+    if n == 0:
+        return 0
+
+    z = 1.96 #1.44 = 85%, 1.96 = 95%
+    phat = float(ups) / n
+    return ((phat + z*z/(2*n) - z * sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n))
